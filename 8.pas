c43267987e69f8b4281f8514416d45bee0d33a1f
Fileger <144237631+Fileger@users.noreply.github.com>
2023-09-06 15:16:45 +0300

    Add files via upload

---
+++ 8.pas
@@ -0,0 +1,22 @@
+﻿var y, x, a, b, c, z: integer;
+begin
+writeln('Введите три числа');
+read(a);
+read(b);
+read(c);
+if (a<c) and (a<b) then x:=a
+else
+if (c<b) and (c<a) then x:=c
+else x:= b;
+ 
+if (b>a) and (b>c) then z:=b
+else
+if (c>a) and (c>b) then z:=c
+else z:=a;
+write('Числа по возрастания = ');
+write (x:3);
+if (a<>x) and (a<>z) then write(a:3);
+if (b<>x) and(b<>z) then write(b:3);
+if (c<>x) and (c<>z) then write(b:3);
+write (z:3);
+end.
